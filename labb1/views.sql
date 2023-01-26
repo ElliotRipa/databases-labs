be8f0a891d31f3f2df545dda4120001750c141a9
ElliotRipa <elliot.ripa@gmail.com>
2023-01-26 16:53:37 +0100

    Started implementation of the BasicInformation view.

--- labb1/views.sql
+++ labb1/views.sql
@@ -1,4 +1,13 @@
+DROP VIEW BasicInformation;
+
 CREATE VIEW BasicInformation AS
-    SELECT idnr, name, login, program
-    FROM Students
-    WHERE true;
+    SELECT Students.idnr, Students.name, Students.login, Students.program, StudentBranches.branch
+    FROM Students, StudentBranches
+    WHERE StudentBranches.student = Students.idnr;
+
+/*CREATE VIEW BasicInformation AS
+SELECT table1.column1,
+       table2.column2
+FROM
+    table1, table2
+WHERE table1.column1 = table2.column1;*/
