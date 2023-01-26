c9f61973141c6c885c06f22359ae0e6803236b2a
ElliotRipa <elliot.ripa@gmail.com>
2023-01-26 16:18:03 +0100

    Added tables.sql

--- labb1/tables.sql
+++ labb1/tables.sql
@@ -13,76 +13,147 @@ SET client_min_messages TO NOTICE; -- More talk
 
 
 
-
 CREATE TABLE Students (
-    idnr TEXT PRIMARY KEY NOT NULL CHECK(LENGTH(idnr) = 10),
-    name TEXT NOT NULL,
-    login TEXT NOT NULL,
+    idnr    CHAR(10) PRIMARY KEY NOT NULL,
+    name    TEXT NOT NULL,
+    login   TEXT NOT NULL,
     program TEXT NOT NULL
+
 );
+
 
 CREATE TABLE Branches (
+    name     TEXT NOT NULL,
+    program  TEXT NOT NULL,
 
-    name TEXT NOT NULL,
-    program TEXT NOT NULL,
     PRIMARY KEY (name, program)
+
 );
+
 
 CREATE TABLE Courses (
-    code VARCHAR(6) PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    credits INT NOT NULL,
-    department text NOT NULL
+    code        CHAR(6) PRIMARY KEY NOT NULL,
+    name        TEXT NOT NULL,
+    credits     INT NOT NULL,
+    department  TEXT NOT NULL
+
 );
+
 
 CREATE TABLE LimitedCourses(
-    code TEXT PRIMARY KEY NOT NULL,
+    code     CHAR(6) PRIMARY KEY NOT NULL,
     capacity INT NOT NULL CHECK(capacity > 0),
+
     FOREIGN KEY(code) REFERENCES Courses(code)
+
 );
 
+
 CREATE TABLE StudentBranches (
-    student TEXT PRIMARY KEY,
-    branch TEXT,
+    student CHAR(10) PRIMARY KEY NOT NULL,
+    branch  TEXT,
     program TEXT NOT NULL,
+
     FOREIGN KEY(student) REFERENCES Students(idnr),
     FOREIGN KEY(branch, program) REFERENCES Branches(name, program)
+
 );
+
 
 CREATE TABLE Classifications(
     name TEXT PRIMARY KEY NOT NULL
 
 );
 
+
 CREATE TABLE Classified(
-    course TEXT PRIMARY KEY,
-    classification TEXT PRIMARY KEY,
+    course         CHAR(6) NOT NULL,
+    classification TEXT NOT NULL,
 
     PRIMARY KEY(course, classification),
-    FOREIGN KEY(course) REFERENCES Courses(code),
-    FOREIGN KEY(classification) REFERENCES Classifications(name)
 
+    FOREIGN KEY(course) REFERENCES Courses(code),
+    FOREIGN KEY(classification) REFERENCES Classifications(name)   --Weird considering multiple classifications possible.
 );
+
 
 CREATE TABLE MandatoryProgram(
-    course TEXT NOT NULL,
+    course  CHAR(6) NOT NULL,
     program TEXT NOT NULL,
-    PRIMARY KEY(course, program),
+
+    PRIMARY KEY(program, course),
+
     FOREIGN KEY(course) REFERENCES Courses(code)
+
 );
+
 
 CREATE TABLE MandatoryBranch(
-    course TEXT PRIMARY KEY
+    course  CHAR(6) NOT NULL,
+    branch  TEXT NOT NULL,
+    program TEXT NOT NULL,
+
+    PRIMARY KEY(course, branch, program),
+
+    FOREIGN KEY(course) REFERENCES Courses(code),
+    FOREIGN KEY(branch, program) REFERENCES Branches(name, program)
+
+);
+
+
+CREATE TABLE RecommendedBranch(
+    course  CHAR(6) NOT NULL,
+    branch  TEXT NOT NULL,
+    program TEXT NOT NULL,
+
+    PRIMARY KEY (course, branch, program),
+
+    FOREIGN KEY(course) REFERENCES Courses(code),
+    FOREIGN KEY(branch, program) REFERENCES Branches(name, program)
+
+);
+
+
+CREATE TABLE Registered(
+
+    student TEXT NOT NULL,
+    course  CHAR(6) NOT NULL,
+
+    PRIMARY KEY(student, course)
+
+);
+
+
+CREATE TABLE Taken(
+    student CHAR(10) NOT NULL,
+    course CHAR(6) NOT NULL,
+    grade CHAR(1) CHECK(grade IN ('U', '3', '4', '5')),
+
+    PRIMARY KEY(student, course),
+
+    FOREIGN KEY(student) REFERENCES Students(idnr),
     FOREIGN KEY(course) REFERENCES Courses(code)
 
 );
 
-CREATE TABLE ();
-CREATE TABLE ();
-CREATE TABLE ();
-CREATE TABLE ();
-CREATE TABLE ();
 
+CREATE TABLE WaitingList(
+    student  CHAR(10) NOT NULL,
+    course   CHAR(6) NOT NULL,
+    position INT NOT NULL,
+
+    PRIMARY KEY (student, course),
+
+    FOREIGN KEY (student) REFERENCES Students(idnr),
+    FOREIGN KEY (course) REFERENCES LimitedCourses(code)
+
+);
 
 
 SELECT * FROM Students;
+
+
+
+
+
+
