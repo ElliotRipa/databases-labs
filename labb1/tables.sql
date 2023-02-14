1b4be9e87d106b5344ef6fd033a56485d38913c8
ElliotRipa <elliot.ripa@gmail.com>
2023-02-14 17:03:35 +0100

    Moved reset script to reset.sql

--- labb1/tables.sql
+++ labb1/tables.sql
@@ -1,17 +1,3 @@
-/*
--- This script deletes everything in your database
-\set QUIET true
-SET client_min_messages TO WARNING; -- Less talk please.
--- This script deletes everything in your database
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO CURRENT_USER;
--- This line makes psql stop on the first error it encounters
--- You may want to remove this when running tests that are intended to fail
-\set ON_ERROR_STOP ON
-SET client_min_messages TO NOTICE; -- More talk
-\set QUIET false
-*/
 
 
 CREATE TABLE Students (
@@ -153,12 +139,3 @@ CREATE TABLE WaitingList(
     FOREIGN KEY (course) REFERENCES LimitedCourses(code)
 
 );
-
-
-SELECT * FROM Students;
-
-
-
-
-
-
