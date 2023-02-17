a0f9a4d21a5d3132e812d6756787a4995763f5ca
ElliotRipa <elliot.ripa@gmail.com>
2023-02-17 16:11:00 +0100

    Finished labb2\tables.sql

--- labb2/tables.sql
+++ labb2/tables.sql
@@ -1,44 +1,53 @@
-CREATE TABLE Students(
-    idnr    CHAR(10) PRIMARY KEY NOT NULL,
-    login   TEXT NOT NULL UNIQUE,
-    name    TEXT NOT NULL,
-    program TEXT NOT NULL,
-    FOREIGN KEY (program) REFERENCES Programs(name)
+CREATE TABLE Programs(
+    name TEXT PRIMARY KEY NOT NULL
+
 );
 
-CREATE TABLE Programs(
-    name TEXT PRIMARY KEY NOT NULL,
 
-)
+CREATE TABLE Students(
+    idnr    CHAR(10) PRIMARY KEY NOT NULL,
+    login   TEXT NOT NULL,
+    name    TEXT NOT NULL,
+    program TEXT NOT NULL,
+
+    FOREIGN KEY(program) REFERENCES Programs(name)
+
+);
+
 
 CREATE TABLE Branches(
     name TEXT NOT NULL,
     program TEXT NOT NULL,
-    PRIMARY KEY (name, program),
-    FOREIGN KEY (program) REFERENCES Programs(name)
+
+    PRIMARY KEY(name, program),
+
+    FOREIGN KEY(program) REFERENCES Programs(name)
 
 );
 
-CREATE TABLE Courses (
+
+CREATE TABLE Courses(
     code CHAR(6) PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
-    credits INT NOT NULL,
+    credits FLOAT NOT NULL,
     department TEXT NOT NULL
 
 );
 
 
 CREATE TABLE LimitedCourses(
+    course CHAR(6) NOT NULL PRIMARY KEY,
     capacity INT NOT NULL,
-    course INT NOT NULL UNIQUE,
-    FOREIGN KEY (course) REFERENCES Courses(code),
-    PRIMARY KEY(course, code)
+
+    FOREIGN KEY(course) REFERENCES Courses(code)
 
 );
+
 
 CREATE TABLE Classifications(
     name TEXT PRIMARY KEY NOT NULL
 );
+
 
 CREATE TABLE Classified(
     course CHAR(6) NOT NULL,
@@ -50,63 +59,74 @@ CREATE TABLE Classified(
     PRIMARY KEY (course, classification)
 );
 
+
 CREATE TABLE WaitingList(
+    student CHAR(10) NOT NULL,
     course CHAR(6) NOT NULL,
     position INT NOT NULL,
-    student CHAR(10) NOT NULL,
 
     FOREIGN KEY(course) REFERENCES Courses(code),
     FOREIGN KEY(student) REFERENCES Students(idnr)
 
-    PRIMARY KEY(course, position, student)
+    PRIMARY KEY(course, position),
+
+    UNIQUE(course, student)
 
 )
 
 CREATE TABLE PartOfBranch(
     student CHAR(10) PRIMARY KEY NOT NULL,
-    program TEXT NOT NULL,
     branch TEXT NOT NULL,
+    program TEXT NOT NULL,
 
-    FOREIGN KEY (student) REFERENCES Student(idnr),
-    FOREIGN KEY (program, branch) REFERENCES Branches(program, name)
+    FOREIGN KEY(student) REFERENCES Students(idnr),
+    FOREIGN KEY(program, branch) REFERENCES Branches(program, name)
+
 );
 
 CREATE TABLE MandatoryBranchCourses(
-    program TEXT NOT NULL,
-    branch TEXT NOT NULL,
     course CHAR(6) NOT NULL,
+    branch TEXT NOT NULL,
+    program TEXT NOT NULL,
 
     FOREIGN KEY(course) REFERENCES Courses(code),
     FOREIGN KEY(program, branch) REFERENCES Branches(program, name),
-    PRIMARY KEY (program, branch, course)
+
+    PRIMARY KEY(program, branch, course)
 
 );
 
 CREATE TABLE MandatoryProgramCourses(
-    program TEXT PRIMARY KEY NOT NULL,
     course CHAR(6) NOT NULL,
+    program TEXT NOT NULL,
 
-    FOREIGN KEY program REFERENCES Program(name),
-    FOREIGN KEY course REFERENCES Courses(name)
+    FOREIGN KEY(program) REFERENCES Programs(name),
+    FOREIGN KEY(course) REFERENCES Courses(code),
+
+    PRIMARY KEY(program, course)
 
 );
 
+
 CREATE TABLE RecommendedBranchCourses(
-    program TEXT NOT NULL,
-    branch TEXT NOT NULL,
     course CHAR(6) NOT NULL,
+    branch TEXT NOT NULL,
+    program TEXT NOT NULL,
 
     PRIMARY KEY(program, branch, course),
     FOREIGN KEY(program, branch) REFERENCES Branches(program, name)
 
 );
 
+
 CREATE TABLE Registered(
-    student CHAR(10) PRIMARY KEY NOT NULL,
+    student CHAR(10) NOT NULL,
     course CHAR(6) NOT NULL,
 
-    FOREIGN KEY (student) REFERENCES Students(idnr),
-    FOREIGN KEY (course) REFERENCES Courses(code)
+    FOREIGN KEY(student) REFERENCES Students(idnr),
+    FOREIGN KEY(course) REFERENCES Courses(code),
+
+    PRIMARY KEY(student, course)
 
 );
 
@@ -119,6 +139,9 @@ CREATE TABLE Grades(
     UNIQUE(student, course),
 
     FOREIGN KEY(student) REFERENCES Students(idnr),
-    FOREIGN KEY(course) REFERENCES Courses(code)
+    FOREIGN KEY(course) REFERENCES Courses(code),
+
+    CHECK(grade IN ('U', '3', '4', '5'))
 
 );
+
