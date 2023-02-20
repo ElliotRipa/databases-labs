91ea770f9e6df976f9b1b258cb9fe556be86cea8
ElliotRipa <elliot.ripa@gmail.com>
2023-02-20 14:43:12 +0100

    Imported tables.sql

--- part1/tables.sql
+++ part1/tables.sql
@@ -1,61 +1,48 @@
--- This script deletes everything in your database
-\set QUIET true
-SET client_min_messages TO WARNING; -- Less talk please.
--- This script deletes everything in your database
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO CURRENT_USER;
--- This line makes psql stop on the first error it encounters
--- You may want to remove this when running tests that are intended to fail
-\set ON_ERROR_STOP ON
-SET client_min_messages TO NOTICE; -- More talk
-\set QUIET false
-
 
 
 CREATE TABLE Students (
-    idnr    CHAR(10) PRIMARY KEY NOT NULL,
-    name    TEXT NOT NULL,
-    login   TEXT NOT NULL,
-    program TEXT NOT NULL
+                          idnr    CHAR(10) PRIMARY KEY NOT NULL,
+                          name    TEXT NOT NULL,
+                          login   TEXT NOT NULL,
+                          program TEXT NOT NULL
 
 );
 
 
 CREATE TABLE Branches (
-    name     TEXT NOT NULL,
-    program  TEXT NOT NULL,
+                          name     TEXT NOT NULL,
+                          program  TEXT NOT NULL,
 
-    PRIMARY KEY (name, program)
+                          PRIMARY KEY (name, program)
 
 );
 
 
 CREATE TABLE Courses (
-    code        CHAR(6) PRIMARY KEY NOT NULL,
-    name        TEXT NOT NULL,
-    credits     FLOAT NOT NULL,
-    department  TEXT NOT NULL
+                         code        CHAR(6) PRIMARY KEY NOT NULL,
+                         name        TEXT NOT NULL,
+                         credits     FLOAT NOT NULL,
+                         department  TEXT NOT NULL
 
 );
 
 
 CREATE TABLE LimitedCourses(
-    code     CHAR(6) PRIMARY KEY NOT NULL,
-    capacity INT NOT NULL CHECK(capacity > 0),
+                               code     CHAR(6) PRIMARY KEY NOT NULL,
+                               capacity INT NOT NULL CHECK(capacity > 0),
 
-    FOREIGN KEY(code) REFERENCES Courses(code)
+                               FOREIGN KEY(code) REFERENCES Courses(code)
 
 );
 
 
 CREATE TABLE StudentBranches (
-    student CHAR(10) PRIMARY KEY NOT NULL,
-    branch  TEXT,
-    program TEXT NOT NULL,
+                                 student CHAR(10) PRIMARY KEY NOT NULL,
+                                 branch  TEXT NOT NULL,
+                                 program TEXT NOT NULL,
 
-    FOREIGN KEY(student) REFERENCES Students(idnr),
-    FOREIGN KEY(branch, program) REFERENCES Branches(name, program)
+                                 FOREIGN KEY(student) REFERENCES Students(idnr),
+                                 FOREIGN KEY(branch, program) REFERENCES Branches(name, program)
 
 );
 
@@ -67,94 +54,88 @@ CREATE TABLE Classifications(
 
 
 CREATE TABLE Classified(
-    course         CHAR(6) NOT NULL,
-    classification TEXT NOT NULL,
+                           course         CHAR(6) NOT NULL,
+                           classification TEXT NOT NULL,
 
-    PRIMARY KEY(course, classification),
+                           PRIMARY KEY(course, classification),
 
-    FOREIGN KEY(course) REFERENCES Courses(code),
-    FOREIGN KEY(classification) REFERENCES Classifications(name)
+                           FOREIGN KEY(course) REFERENCES Courses(code),
+                           FOREIGN KEY(classification) REFERENCES Classifications(name)
     --Weird considering multiple classifications possible.
 );
 
 
 CREATE TABLE MandatoryProgram(
-    course  CHAR(6) NOT NULL,
-    program TEXT NOT NULL,
+                                 course  CHAR(6) NOT NULL,
+                                 program TEXT NOT NULL,
 
-    PRIMARY KEY(program, course),
+                                 PRIMARY KEY(program, course),
 
-    FOREIGN KEY(course) REFERENCES Courses(code)
+                                 FOREIGN KEY(course) REFERENCES Courses(code)
 
 );
 
 
 CREATE TABLE MandatoryBranch(
-    course  CHAR(6) NOT NULL,
-    branch  TEXT NOT NULL,
-    program TEXT NOT NULL,
+                                course  CHAR(6) NOT NULL,
+                                branch  TEXT NOT NULL,
+                                program TEXT NOT NULL,
 
-    PRIMARY KEY(course, branch, program),
+                                PRIMARY KEY(course, branch, program),
 
-    FOREIGN KEY(course) REFERENCES Courses(code),
-    FOREIGN KEY(branch, program) REFERENCES Branches(name, program)
+                                FOREIGN KEY(course) REFERENCES Courses(code),
+                                FOREIGN KEY(branch, program) REFERENCES Branches(name, program)
 
 );
 
 
 CREATE TABLE RecommendedBranch(
-    course  CHAR(6) NOT NULL,
-    branch  TEXT NOT NULL,
-    program TEXT NOT NULL,
+                                  course  CHAR(6) NOT NULL,
+                                  branch  TEXT NOT NULL,
+                                  program TEXT NOT NULL,
 
-    PRIMARY KEY (course, branch, program),
+                                  PRIMARY KEY (course, branch, program),
 
-    FOREIGN KEY(course) REFERENCES Courses(code),
-    FOREIGN KEY(branch, program) REFERENCES Branches(name, program)
+                                  FOREIGN KEY(course) REFERENCES Courses(code),
+                                  FOREIGN KEY(branch, program) REFERENCES Branches(name, program)
 
 );
 
 
 CREATE TABLE Registered(
 
-    student TEXT NOT NULL,
-    course  CHAR(6) NOT NULL,
+                           student TEXT NOT NULL,
+                           course  CHAR(6) NOT NULL,
 
-    PRIMARY KEY(student, course)
+                           PRIMARY KEY(student, course),
+
+                           FOREIGN KEY(student) REFERENCES Students(idnr),
+                           FOREIGN KEY(course) REFERENCES Courses(code)
 
 );
 
 
 CREATE TABLE Taken(
-    student CHAR(10) NOT NULL,
-    course CHAR(6) NOT NULL,
-    grade CHAR(1) CHECK(grade IN ('U', '3', '4', '5')),
+                      student CHAR(10) NOT NULL,
+                      course CHAR(6) NOT NULL,
+                      grade CHAR(1) NOT NULL CHECK(grade IN ('U', '3', '4', '5')),
 
-    PRIMARY KEY(student, course),
+                      PRIMARY KEY(student, course),
 
-    FOREIGN KEY(student) REFERENCES Students(idnr),
-    FOREIGN KEY(course) REFERENCES Courses(code)
+                      FOREIGN KEY(student) REFERENCES Students(idnr),
+                      FOREIGN KEY(course) REFERENCES Courses(code)
 
 );
 
 
 CREATE TABLE WaitingList(
-    student  CHAR(10) NOT NULL,
-    course   CHAR(6) NOT NULL,
-    position INT NOT NULL,
+                            student  CHAR(10) NOT NULL,
+                            course   CHAR(6) NOT NULL,
+                            position INT NOT NULL,
 
-    PRIMARY KEY (student, course),
+                            PRIMARY KEY (student, course),
 
-    FOREIGN KEY (student) REFERENCES Students(idnr),
-    FOREIGN KEY (course) REFERENCES LimitedCourses(code)
+                            FOREIGN KEY (student) REFERENCES Students(idnr),
+                            FOREIGN KEY (course) REFERENCES LimitedCourses(code)
 
 );
-
-
-SELECT * FROM Students;
-
-
-
-
-
-
